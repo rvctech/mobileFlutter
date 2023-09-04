a0841f6e9b4487b2922fb35aa5eb1dd9e5de7f4d
Cornelius-Mutisya <corneliusmutisya11@gmail.com>
2023-09-04 18:02:44 +0300

    Reset pass screen refactor

--- lib/screens/auth/forgotpassword.dart
+++ lib/screens/auth/forgotpassword.dart
@@ -1,74 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:tunyce/widgets/primary_button.dart';
+import 'package:tunyce/widgets/text_input.dart';
 
-class ForgotPasswordScreen  extends StatelessWidget {
+class ForgotPasswordScreen extends StatelessWidget {
+  const ForgotPasswordScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Forgot Password'), 
-        backgroundColor: Colors.redAccent,
+        title: const Text('Forgot Password'),
       ),
-      body: ForgotPasswordBody(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.jpg',
+                width: 220,
+              ),
+              const SizedBox(height: 30),
+              const TextInputWidget(
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Colors.redAccent,
+                ),
+                hintText: 'Enter your email',
+                labelText: 'Email',
+              ),
+              const SizedBox(height: 20),
+              PrimaryButtonWidget(
+                label: 'Reset Password',
+                onTap: () async {},
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-class ForgotPasswordBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        constraints: BoxConstraints.expand(),
-        color: Colors.black,
-        child:SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset('assets/images/logo.jpg', width: 220, height: 240),
-                // Your logo here
-              ),
-              const SizedBox(height: 20),
-
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 32.0),
-                child: TextField(
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Colors.redAccent,
-                    ),
-                    hintText: 'Enter your Email address',
-                    hintStyle: TextStyle(color: Colors.grey),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.redAccent),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Implement your sign-up logic here
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white, backgroundColor: Colors.redAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: const Text('Reset Password'),
-
-                ),
-              ),
-            ],
-          ),
-        ));
-
-  }
-}
-
